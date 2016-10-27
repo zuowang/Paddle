ce08baa171fc8a726075aabd47561dc1c8e6d26d
Zuo Wang <wangzuo.nj@qq.com>
2016-10-27 17:54:07 +0800

    fix optimizer

--- paddle/parameter/FirstOrderOptimizer.cpp
+++ paddle/parameter/FirstOrderOptimizer.cpp
@@ -181,6 +181,8 @@ void RMSPropParameterOptimizer::update(const VectorPtr vecs[],
     t0Vec_[sparseId] = timer_ + 1;
   }
 
+  vecs[PARAMETER_GRADIENT]->add(*vecs[PARAMETER_GRADIENT_AVG], 1.0f);
+
   // E(g_t^2) = \rou * E(g_{t-1}^2) + (1-\rou) * g^2
   // For the first time update, make the sum be the current square
   // so that the initial estimation of E(g_t^2) will not be too small.
